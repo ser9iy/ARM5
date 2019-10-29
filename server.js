3f7af93f5057cf426d71c34dca5c093c4657f06c
Sergiy <tsyb.sergiy@gmail.com>
2019-10-29 13:52:43 +0200

    chat
    chat

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 const TelegramBot = require('node-telegram-bot-api');
-const token = '743684070:AAHERNRtWQaZHi2ROJXdiRiLdarlbgZJTQc';
+const token = '940524194:AAEyD1AUytEBH7qqP6Q6gM-6pXPYOo_PTY8';
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
@@ -16,8 +16,8 @@ var dbUsers = require('./db');
 
 
 passport.use(new Strategy(
-  function(username, password, cb) {
-   dbUsers.users.findByUsername(username, function(err, user) {
+  function (username, password, cb) {
+    dbUsers.users.findByUsername(username, function (err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       if (user.password != password) { return cb(null, false); }
@@ -33,11 +33,11 @@ passport.use(new Strategy(
 // typical implementation of this is as simple as supplying the user ID when
 // serializing, and querying the user record by ID from the database when
 // deserializing.
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
+passport.deserializeUser(function (id, cb) {
   dbUsers.users.findById(id, function (err, user) {
     if (err) { return cb(err); }
     cb(null, user);
@@ -57,18 +57,18 @@ MongoClient.connect('mongodb://serg:1234@ds125288.mlab.com:25288/armlist', (err,
 
 
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
 };
 
 
@@ -79,7 +79,7 @@ app.set('views', __dirname + '/views');
 app.use(allowCrossDomain);
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(bodyParser.json())
 app.use(express.static('public'))
@@ -90,174 +90,174 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //BOT
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 bot.onText(/\/start/, (msg, match) => {
-  
+
 
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
-  const id=msg.chat.id;
-  fs.appendFile('chatids.txt', id+'-', function (err) {
+  const id = msg.chat.id;
+  fs.appendFile('chatids.txt', id + '-', function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, 'Бот почав роботу, будь-які +/- зміни вище певної суми будуть відправлені. Бот на стадії розробки.Відправивши звичайне повідомлення отримаєте дані по залах'+'.Ваш id:'+id);
+  bot.sendMessage(chatId, 'Бот почав роботу, будь-які +/- зміни вище певної суми будуть відправлені. Бот на стадії розробки.Відправивши звичайне повідомлення отримаєте дані по залах' + 'ВАШЕ ІД->>>>>>>>:' + id);
 });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  const id=msg.chat.id
+  const id = msg.chat.id
   // send a message to the chat acknowledging receipt of their message
   var content;
- /* db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    for(var i=0; i<result.length; i++){
-    bot.sendMessage(chatId, result[i].name+' '+result[i].quote+'  Картки: '+result[i].cardsCash);
-    }
-  });*/
-  
- // debug('__________________________mess________');
+  /* db.collection('quotes').find().toArray((err, result) => {
+     if (err) return console.log(err)
+     for(var i=0; i<result.length; i++){
+     bot.sendMessage(chatId, result[i].name+' '+result[i].quote+'  Картки: '+result[i].cardsCash);
+     }
+   });*/
 
-  bot.sendMessage(chatId,'v.2.0 ok id:'+id);
+  // debug('__________________________mess________');
+
+  bot.sendMessage(chatId, 'v.2.0 ok ВАШЕ ІД->>>>>>>>:' + id);
 
 });
 //BOT END
 
-var timerId = setInterval(function() {
+var timerId = setInterval(function () {
   //bot.sendMessage(626376656, 'Timer');
   //bot.sendMessage(481503296, 'Login');
- // var dani=[];
+  // var dani=[];
   db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
-   // bot.sendMessage(626376656, 'STARTED');
-    
-    for(var i=0; i<result.length; i++){
+    // bot.sendMessage(626376656, 'STARTED');
+
+    for (var i = 0; i < result.length; i++) {
       //bot.sendMessage(626376656, 'q');
-      
-      if (result[i].quote.indexOf("+") !== -1){
-        var know=parseInt(result[i].quote.substr(1));
-        var last=parseInt(result[i].lt);
-        var z=30000;
-        var t=10000;
-        if((result[i].name=='Живова 7')||(result[i].name=='Обнова'))
-        {
-          z=45000;
+
+      if (result[i].quote.indexOf("+") !== -1) {
+        var know = parseInt(result[i].quote.substr(1));
+        var last = parseInt(result[i].lt);
+        var z = 30000;
+        var t = 10000;
+        if ((result[i].name == 'Живова 7') || (result[i].name == 'Обнова')) {
+          z = 45000;
         }
         //bot.sendMessage(626376656, 'know:'+know+'last:'+last);
-        
-          if((know>z)&&(last<1)){
-        db.collection('quotes')
-          .findOneAndUpdate({name: result[i].name}, {
-            $set: {              
-              lt:know,           
-              
-            }
-            
-          });
-          bot.sendMessage(626376656,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-          bot.sendMessage(464115643,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-          bot.sendMessage(481503296,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-          bot.sendMessage(690655842,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-          
+
+        if ((know > z) && (last < 1)) {
+          db.collection('quotes')
+            .findOneAndUpdate({ name: result[i].name }, {
+              $set: {
+                lt: know,
+
+              }
+
+            });
+          bot.sendMessage(626376656, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(464115643, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(854071253, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(690655842, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+
         }
-      
 
-        else if((know>z)&&((know-last)>=t)){
-        db.collection('quotes')
-          .findOneAndUpdate({name: result[i].name}, {
-            $set: {              
-              lt:know,           
-              
-            }
-            
-          });
-        bot.sendMessage(626376656,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-        bot.sendMessage(464115643,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-          bot.sendMessage(481503296,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-          bot.sendMessage(690655842,'Зверніть увагу: '+result[i].name+':+'+know+' '+' На картках:'+result[i].cardsCash);
-      }
 
-        if((know<z)&&(last>2)){
-        db.collection('quotes')
-        .findOneAndUpdate({name: result[i].name}, {
-          $set: {              
-            lt:0,           
-            
-          }
-          
-        });
-      }
-        
-        
-      }
-   
-      if (result[i].quote.indexOf("-") !== -1    ){
-        var know=parseInt(result[i].quote.substr(1));
-        var last=parseInt(result[i].quote.lt);
-        var z=30000;
-        var t=10000;
-        if((result[i].name=='Живова 7')||(result[i].name=='Обнова'))
-        {
-          z=45000;
-        } 
+        else if ((know > z) && ((know - last) >= t)) {
+          db.collection('quotes')
+            .findOneAndUpdate({ name: result[i].name }, {
+              $set: {
+                lt: know,
 
-          if((know>z)&&(last<2)){
-        db.collection('quotes')
-          .findOneAndUpdate({name: result[i].name}, {
-            $set: {              
-              lt:know,           
-              
-            }
-            
-          });
-          bot.sendMessage(626376656,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
-          bot.sendMessage(464115643,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
-          bot.sendMessage(481503296,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
-          bot.sendMessage(690655842,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
-          
+              }
+
+            });
+          bot.sendMessage(626376656, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(464115643, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(481503296, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(690655842, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(854071253, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
         }
-      
 
-        else if((know>z)&&((know-last)>=t)){
-        db.collection('quotes')
-          .findOneAndUpdate({name: result[i].name}, {
-            $set: {              
-              lt:know,           
-              
-            }
-            
-          });
-          bot.sendMessage(626376656,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
-          bot.sendMessage(464115643,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
-          bot.sendMessage(481503296,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
-          bot.sendMessage(690655842,'Зверніть увагу: '+result[i].name+':-'+know+' '-' На картках:'+result[i].cardsCash);
+        if ((know < z) && (last > 2)) {
+          db.collection('quotes')
+            .findOneAndUpdate({ name: result[i].name }, {
+              $set: {
+                lt: 0,
+
+              }
+
+            });
+        }
+
+
       }
 
-        if((know<z)&&(last>z)){
-        db.collection('quotes')
-        .findOneAndUpdate({name: result[i].name}, {
-          $set: {              
-            lt:0,           
-            
-          }
-          
-        });
+      if (result[i].quote.indexOf("-") !== -1) {
+        var know = parseInt(result[i].quote.substr(1));
+        var last = parseInt(result[i].quote.lt);
+        var z = 30000;
+        var t = 10000;
+        if ((result[i].name == 'Живова 7') || (result[i].name == 'Обнова')) {
+          z = 45000;
+        }
+
+        if ((know > z) && (last < 2)) {
+          db.collection('quotes')
+            .findOneAndUpdate({ name: result[i].name }, {
+              $set: {
+                lt: know,
+
+              }
+
+            });
+          bot.sendMessage(626376656, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(464115643, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(481503296, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(690655842, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(854071253, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+        }
+
+
+        else if ((know > z) && ((know - last) >= t)) {
+          db.collection('quotes')
+            .findOneAndUpdate({ name: result[i].name }, {
+              $set: {
+                lt: know,
+
+              }
+
+            });
+          bot.sendMessage(626376656, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(464115643, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(481503296, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(690655842, 'Зверніть увагу: ' + result[i].name + ':-' + know + ' ' - ' На картках:' + result[i].cardsCash);
+          bot.sendMessage(854071253, 'Зверніть увагу: ' + result[i].name + ':+' + know + ' ' + ' На картках:' + result[i].cardsCash);
+        }
+
+        if ((know < z) && (last > z)) {
+          db.collection('quotes')
+            .findOneAndUpdate({ name: result[i].name }, {
+              $set: {
+                lt: 0,
+
+              }
+
+            });
+        }
+
+
+
       }
-        
-        
-        
-      }
-    
-        
-      
-     
+
+
+
+
     }
-    
+
   })
-  
+
 }, 60000);
 
 
@@ -271,53 +271,53 @@ var timerId = setInterval(function() {
 
 
 app.get('/',
-  function(req, res) {
-     res.redirect('/login');
+  function (req, res) {
+    res.redirect('/login');
   });
 
 app.get('/login',
-  function(req, res){
-   
+  function (req, res) {
+
     res.render('login');
   });
-  
-app.post('/login', 
+
+app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
+  function (req, res) {
     res.redirect('/dashboard');
   });
-  
+
 app.get('/logout',
-  function(req, res){
+  function (req, res) {
     req.logout();
     res.redirect('/login');
   });
 
 
-app.get('/dashboard',require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
-  
+app.get('/dashboard', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
+
   db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
-    for(var i=0;i<result.length;i++){
+    for (var i = 0; i < result.length; i++) {
       db.collection('quotes')
-  .findOneAndUpdate({name: req.body.name}, {
-    $set: {
-      
-      lt:2 ,
-      
-      
-     
-      
+        .findOneAndUpdate({ name: req.body.name }, {
+          $set: {
+
+            lt: 2,
+
+
+
+
+          }
+        })
     }
-  })
-    }
-    res.render('index.ejs', {quotes: result})
+    res.render('index.ejs', { quotes: result })
   })
 })
-app.get('/chort',require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
+app.get('/chort', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
   db.collection('sport').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('chort.ejs', {quotes: result})
+    res.render('chort.ejs', { quotes: result })
   })
 })
 
@@ -338,68 +338,68 @@ app.post('/sport-quotes', (req, res) => {
 
 app.put('/quotes', (req, res) => {
   db.collection('quotes')
-  .findOneAndUpdate({name: req.body.name}, {
-    $set: {
-      
-      quote: req.body.quote,
-      time:req.body.time,
-      cardsCash:req.body.cardsCash,
-     
-      
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({ name: req.body.name }, {
+      $set: {
+
+        quote: req.body.quote,
+        time: req.body.time,
+        cardsCash: req.body.cardsCash,
+
+
+      }
+    }, {
+        sort: { _id: -1 },
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
 })
 app.delete('/sport-quotes', (req, res) => {
-  db.collection('sport').findOneAndDelete({name: req.body.name}, (err, result) => {
+  db.collection('sport').findOneAndDelete({ name: req.body.name }, (err, result) => {
     if (err) return res.send(500, err)
     res.send('A darth vadar quote got deleted')
   })
 })
 app.put('/sport-quotes', (req, res) => {
   db.collection('sport')
-  .findOneAndUpdate({name: req.body.name}, {
-    $set: {
-      
-      quote: req.body.quote,
-      credit:req.body.credit,
-      
-     
-      
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({ name: req.body.name }, {
+      $set: {
+
+        quote: req.body.quote,
+        credit: req.body.credit,
+
+
+
+      }
+    }, {
+        sort: { _id: -1 },
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
 })
 app.put('/quotes/zmina', (req, res) => {
   db.collection('quotes')
-  .findOneAndUpdate({name: req.body.name}, {
-    $set: {
-      
-      
-      zmina:req.body.zmina
-      
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({ name: req.body.name }, {
+      $set: {
+
+
+        zmina: req.body.zmina
+
+      }
+    }, {
+        sort: { _id: -1 },
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
 })
 
 app.delete('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+  db.collection('quotes').findOneAndDelete({ name: req.body.name }, (err, result) => {
     if (err) return res.send(500, err)
     res.send('A darth vadar quote got deleted')
   })
